5972554dd117ee1dfbc2fa06f28c422aba4dd1e0
Samuel Bonet <95564083+samuelbonet@users.noreply.github.com>
2024-09-28 13:50:18 +0000

    create deep.py exercise

---
+++ psets1/deep.py
@@ -0,0 +1,11 @@
+phrase= input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+if phrase=="42":
+    print("Yes")
+elif phrase=="Forty Two":
+    print("Yes")
+elif phrase=="forty-two":
+    print("Yes")
+else:
+    print("No")
+
